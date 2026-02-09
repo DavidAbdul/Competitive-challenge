04161f4865e245a44869096b6e59623a1fe27102
DavidAbdul <d.abdul82@hotmail.com>
2026-02-09 13:38:38 -0600

    solucion

--- challenges/challenge1-two-sum/solution.js
+++ challenges/challenge1-two-sum/solution.js
@@ -1,18 +1,29 @@
 /**
  * Two Sum
- * 
+ *
  * @param {number[]} nums - Array de números enteros
  * @param {number} target - Número objetivo
  * @return {number[]} - Índices de los dos números que suman target
- * 
+ *
  * Ejemplos:
  * twoSum([2,7,11,15], 9) => [0,1]
  * twoSum([3,2,4], 6) => [1,2]
  * twoSum([3,3], 6) => [0,1]
  */
 function twoSum(nums, target) {
-    // ESCRIBE TU CÓDIGO AQUÍ
-    
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
 }
 
 module.exports = twoSum;
